ed7e9ed8a49b6899d862ec5bbd42a7768b147957
mohakjl <mohamedkoujil75@gmail.com>
2024-01-11 23:47:43 +0100

    Canvis: - funció que genera un historial de factures - funcio que permet veure aquestes factures

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit
 /**
  * Aquesta funció rep el consum i retorna el cost per litre segons el consum
  * @author mohamedkoujil
@@ -82,4 +83,89 @@ fun formatMoneda(amount: Double): String {
 fun formatPercentage(percentage: Double): String {
     return "${(percentage * 100)}%"
 }
+
+/**
+ * Aquesta funcio converteix les dades de l'usuari en un array
+ * @author mohamedkoujil
+ * @since 11/01/2023
+ * @param quotaFixa Quota fixa
+ * @param quotaVariable Quota variable
+ * @param consum Consum
+ * @param descompteBS Descompte del bo social
+ * @param descompteFN Descompte de familia nombrosa o monoparental
+ * @param contFactures Contador de factures
+ * @return Array amb les dades de la factura
+ */
+fun historialFactures(quotaFixa: Double, quotaVariable: Double, consum: Int, descompteBS: Double, descompteFN: Double, contFactures: Int): Array<Double> {
+    var totalSenseDescomptes = quotaVariable + quotaFixa
+    var descomptes = (totalSenseDescomptes * descompteFN) + (totalSenseDescomptes * descompteBS)
+    var total = totalSenseDescomptes - descomptes
+
+    var novaFactura = arrayOf(
+        contFactures.toDouble(),
+        quotaFixa,
+        quotaVariable,
+        consum.toDouble(),
+        totalSenseDescomptes,
+        descompteFN,
+        descompteBS,
+        total)
+    return novaFactura
+}
+
+/**
+ * Aquesta funcio imprimeix la factura segons la factura seleccionada per l'usuari en l'historial de factures
+ * @author mohamedkoujil
+ * @since 11/01/2023
+ * @param historialFactures Historial de factures
+ * @param facturaUsr Factura seleccionada per l'usuari
+ */
+fun facturaEnHistorialUsr (historialFactures: MutableList<Array<Double>>, facturaUsr: Int) {
+    var factura = facturaUsr-1
+    println("           FACTURA")
+    println("---------------------------")
+    println("- Quota fixa:        ${formatMoneda(historialFactures[factura][1])}")
+    println("- Quota variable:    ${formatMoneda(historialFactures[factura][2])}")
+    println("- Consum:            ${historialFactures[factura][3].toInt()} L")
+    println("- Total Brut:        ${formatMoneda(historialFactures[factura][4])}")
+    println("- Descompte FN:      ${formatPercentage(historialFactures[factura][5])}")
+    println("- Descompte BS:      ${formatPercentage(historialFactures[factura][6])}")
+    println("                     ------")
+    println("- TOTAL:             ${formatMoneda(historialFactures[factura][7])}")
+    println("---------------------------")
+}
+
+/**
+ * Aquesta funcio imprimeix l'historial de factures i permet seleccionar una factura per veure-la amb més detall o tornar al menu principal si no hi ha cap factura en l'historial de factures
+ * @author mohamedkoujil
+ * @since 11/01/2023
+ * @param historialFactures Historial de factures
+ */
+fun veureHistorial(historialFactures: MutableList<Array<Double>>) {
+    if (historialFactures.isEmpty()) {
+        println(RED + "No hi ha cap factura en l'historial" + RESET)
+        TimeUnit.SECONDS.sleep(2)
+    } else {
+        do {
+            var facturaUsr = readInt(
+                "Seleccioni una opció:\n" +
+                buildString {
+                    for (i in historialFactures.indices) {
+                        append("${i + 1}. Factura num: ${i + 1}\n")
+                    }
+                } +
+                        "${historialFactures.lastIndex + 2}. Tornar al menu",
+                "Introdueixi un número",
+                "Factura no existent",
+                1,
+                historialFactures.lastIndex + 2
+            )
+            if (facturaUsr == historialFactures.lastIndex + 2) break
+            facturaEnHistorialUsr(historialFactures, facturaUsr)
+            TimeUnit.SECONDS.sleep(2)
+        } while (true)
+    }
+}
+
+
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,69 @@
+import java.util.concurrent.TimeUnit
 fun main() {
+    var historial: MutableList<Array<Double>> = mutableListOf()
+    var contFactures = 0
     do {
         var quotaFixa = 6.0
         var quotaVariable = 0.0
         var descompteFN = 0.0
         var descompteBS = 0.0
-        var menu = readInt(
-            "Benvingut a la calculadora de factura d'aigua\n" +
-                    "1. Calcular factura d'aigua\n" +
-                    "2. Sortir\n",
-            "Introdueixi un nombre vàlid",
-            "Introdueixi una opció vàlida",
-            1,
-            2
-        )
-        if (menu == 2) break
+
+        var menu: Int
+        do {
+            menu = readInt(
+                "Benvingut a la calculadora de factura d'aigua\n" +
+                        "1. Calcular factura d'aigua\n" +
+                        "2. Historial de factures\n" +
+                        "3. Sortir",
+                "Introdueixi un nombre vàlid",
+                "Introdueixi una opció vàlida",
+                1,
+                3
+            )
+
+            when (menu) {
+                2 -> {
+                    if (historial.isEmpty()) {
+                        println(RED + "No hi ha cap factura en l'historial" + RESET)
+                        TimeUnit.SECONDS.sleep(2)
+                    } else {
+                        veureHistorial(historial)
+                    }
+                }
+                1 -> break
+            }
+        } while (menu != 3)
+
+        //Si es tria la opció 3, es surt del programa
+        if (menu == 3) break
 
         var consum = readInt(
-            "Introdueixi el seu consum en litres\n",
+            "Introdueixi el seu consum en litres",
             "Introdueixi un número",
             "Introdueixi un nombre vàlid",
             1,
             Int.MAX_VALUE
         )
 
+        //Es calcula el cost per litre i la quota variable
         var costLitre = costLitre(consum)
         quotaVariable = costLitre * consum
 
+        //Es pregunta si es posseeix algun dels titols
         var descompte = readInt(
             "Posseeixes algun d'aquests titols?\n" +
                     YELLOW + "Recordatori: Si es tenen els 2 titols, només s'aplicarà el descompte del bo social\n" + RESET +
                     "1. Familia nombrosa o monoparental\n" +
                     "2. Bo Social\n" +
                     "3. Els 2\n" +
-                    "4. No poseeixo cap tipus d'ajuda\n",
+                    "4. No poseeixo cap tipus d'ajuda",
             "Introdueixi un numero",
             "Introdueixi un nombre valid",
             1,
             4
         )
 
+        //Es calcula el descompte depenent dels titols que es posseeixen
         when (descompte) {
             1 -> {
                 var membresFamilia = readInt("Membres de la familia", "Introdueixi un nombre", "Introdueixi un nombre valid", 1, 50)
@@ -55,6 +80,12 @@ fun main() {
                 quotaFixa = 3.0
             }
         }
+
         factura(quotaFixa, quotaVariable, consum, descompteBS, descompteFN)
+        TimeUnit.SECONDS.sleep(2)
+        historial.add(historialFactures(quotaFixa, quotaVariable, consum, descompteBS, descompteFN, contFactures))
+        contFactures++
+
     }while (true)
+
 }
